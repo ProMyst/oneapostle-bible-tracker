1947573d6fb4b9cd55588b7bffd0d1b213496426
Matthew Miller <mm484882@gmail.com>
2024-12-24 15:14:59 -0600

    Delete OneApostle 4 directory

--- OneApostle 4/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold text-center text-[#4338CA]">Welcome to OneApostle</h1>
-      <p className="text-center mt-4">Start tracking your Bible reading progress today!</p>
-    </div>
-  )
-}
-
